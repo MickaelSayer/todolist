edf6d73f6e183fc0459418604545bd238c4214b2
freaks <mickael.sayer.dev@gmail.com>
2023-11-10 18:03:09 +0100

    unninstale bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,6 @@
 import { registerReactControllerComponents } from '@symfony/ux-react';
 import './bootstrap.js';
 import './styles/app.scss';
-import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
